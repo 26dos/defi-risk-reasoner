a8cd54b4d2a81d4a5e24ea4df121c9240d54df47
Wei Han Tan <120706474+26dos@users.noreply.github.com>
2025-01-04 22:41:18 +0800

    scenarios: include current state baseline

--- src/risk_reasoner/tools/scenarios.py
+++ src/risk_reasoner/tools/scenarios.py
@@ -3,11 +3,13 @@ from .liquidation import project_health_factor
 
 
 SCENARIOS = [
-    {"name": "eth_-20%", "collateral_change_pct": -20.0},
-    {"name": "eth_-30%", "collateral_change_pct": -30.0},
-    {"name": "eth_-50%", "collateral_change_pct": -50.0},
-    {"name": "stablecoin_depeg_-10%", "debt_change_pct": -10.0},
-    {"name": "stablecoin_depeg_+5%", "debt_change_pct": 5.0},
+    {"name": "current",                "collateral_change_pct": 0.0,  "debt_change_pct": 0.0},
+    {"name": "eth_-20%",               "collateral_change_pct": -20.0},
+    {"name": "eth_-30%",               "collateral_change_pct": -30.0},
+    {"name": "eth_-50%",               "collateral_change_pct": -50.0},
+    {"name": "stablecoin_depeg_-10%",  "debt_change_pct": -10.0},
+    {"name": "stablecoin_depeg_+5%",   "debt_change_pct": 5.0},
+    {"name": "tail_collateral_-70%",   "collateral_change_pct": -70.0},
 ]
 
 
